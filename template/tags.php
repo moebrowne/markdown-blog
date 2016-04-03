0e7316681284e2c0422e8a1c6678d2f15ba04cc3
MoeBrowne <oliver.browne@uplyme.com>
2016-04-03 01:04:04 +0100

    Added a tag sidebar

---
+++ template/tags.php
@@ -0,0 +1,5 @@
+<aside class="tags">
+    <?php foreach ($tags as $name => $count) : ?>
+        <a class="tag" data-count="<?= $count; ?>" href="/tags/<?= $name; ?>/"><?= $name; ?></a>
+    <?php endforeach; ?>
+</aside>
