42a58861fcf8bb158f7df8460862006f0c0c02a2
MoeBrowne <oliver.browne@uplyme.com>
2016-04-09 00:32:31 +0100

    Updated the post template to include the supporting templates

--- template/posts.php
+++ template/posts.php
@@ -1,10 +1,13 @@
+<?php include "head.html"; ?>
 <div class="posts">
     <div class="wrapper">
     <?php
     foreach ($posts as $post) {
-        echo $post;
+        include('post/summary.php');
     }
     ?>
-    <?php include "template/pagination.php"; ?>
+    <?php include "pagination.php"; ?>
     </div>
 </div>
+<?php include "tags.php"; ?>
+<?php include "foot.html"; ?>
