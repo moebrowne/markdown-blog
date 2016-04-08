206677d70674ad8a904477e4c3431a55350b4d08
MoeBrowne <oliver.browne@uplyme.com>
2016-04-09 00:33:24 +0100

    Moved the header and footer to its own includes directory

--- template/posts.php
+++ template/posts.php
@@ -1,4 +1,4 @@
-<?php include "head.html"; ?>
+<?php include "includes/head.html"; ?>
 <div class="posts">
     <div class="wrapper">
     <?php
@@ -10,4 +10,4 @@
     </div>
 </div>
 <?php include "tags.php"; ?>
-<?php include "foot.html"; ?>
+<?php include "includes/foot.html"; ?>
