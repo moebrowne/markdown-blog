51b5a8977cd9a1f160f45af2076b2a1d32b6ab2e
MoeBrowne <oliver.browne@uplyme.com>
2016-04-16 22:09:58 +0100

    Corrected the tag aside links

--- template/includes/tags.php
+++ template/includes/tags.php
@@ -1,5 +1,7 @@
 <aside class="tags">
     <?php foreach ($tags as $name => $count) : ?>
-        <a class="tag" data-count="<?= $count; ?>" href="/tags/<?= $name; ?>"><?= $name; ?></a>
+        <a class="tag" data-count="<?= $count; ?>" href="/tags/<?= str_replace(' ', '-', $name); ?>">
+            <?= $name; ?>
+        </a>
     <?php endforeach; ?>
 </aside>
