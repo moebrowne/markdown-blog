1f434c42adf6e5c0b579838c1b7dd9d297dde72b
MoeBrowne <oliver.browne@uplyme.com>
2016-04-17 00:37:45 +0100

    Added a meta description

--- template/includes/head.php
+++ template/includes/head.php
@@ -9,6 +9,7 @@
     <?php require 'GoogleAnalytics.php'; ?>
     <meta property="og:site_name" itemprop="name" content="Mountain Of Code">
     <meta property="og:url" itemprop="url" content="https://www.mountainofcode.co.uk/">
+    <meta name="description" property="og:description" itemprop="description" content="Mountain of code is a collection of web and dev ops related projects mixed with a little hacking and tinkering.">
 </head>
 <body>
 
