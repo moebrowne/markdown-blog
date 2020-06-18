1a9aba891c3574310fc869a49d3c3698db30d359
MoeBrowne <moebrowne@users.noreply.github.com>
2020-06-18 17:20:59 +0100

    Allows GA to be optional

--- template/includes/head.php
+++ template/includes/head.php
@@ -6,7 +6,7 @@
 	<link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono|Ubuntu+Mono|Ubuntu:400,700' rel='stylesheet' type='text/css'>
 	<link href='/css/style.css' rel='stylesheet' type='text/css'>
 	<link href='/css/prism.css' rel='stylesheet' type='text/css'>
-    <?php require 'GoogleAnalytics.php'; ?>
+    <?php if(empty($GoogleAnalyticsUA) === false) { require 'GoogleAnalytics.php'; } ?>
     <meta property="og:site_name" itemprop="name" content="Mountain Of Code">
     <meta property="og:url" itemprop="url" content="https://www.mountainofcode.co.uk/">
     <meta name="description" property="og:description" itemprop="description" content="Mountain of code is a collection of web and dev ops related projects mixed with a little hacking and tinkering.">
