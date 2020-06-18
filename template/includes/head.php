d34d2765d40cfcae922399901285dd17dc5e00c5
MoeBrowne <moebrowne@users.noreply.github.com>
2020-06-18 17:46:35 +0100

    Replaces tabs with spaces

--- template/includes/head.php
+++ template/includes/head.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html lang="en" itemscope itemtype="http://schema.org/Blog">
 <head>
-	<meta charset="UTF-8">
-	<title>Mountain Of Code</title>
+    <meta charset="UTF-8">
+    <title>Mountain Of Code</title>
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Share+Tech+Mono|Ubuntu+Mono|Ubuntu:400,700&display=swap" />
-	<link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono|Ubuntu+Mono|Ubuntu:400,700&display=swap' rel='stylesheet' type='text/css'>
-	<link href='/css/style.css' rel='stylesheet' type='text/css'>
-	<link href='/css/prism.css' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Share+Tech+Mono|Ubuntu+Mono|Ubuntu:400,700&display=swap' rel='stylesheet' type='text/css'>
+    <link href='/css/style.css' rel='stylesheet' type='text/css'>
+    <link href='/css/prism.css' rel='stylesheet' type='text/css'>
     <?php if(empty($GoogleAnalyticsUA) === false) { require 'GoogleAnalytics.php'; } ?>
     <meta property="og:site_name" itemprop="name" content="Mountain Of Code">
     <meta property="og:url" itemprop="url" content="https://www.mountainofcode.co.uk/">
